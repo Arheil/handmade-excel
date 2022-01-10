8622c0b19cdb3797a6cd1a54a9931ed8804d07de
Stanislav Verman <verman19837@gmail.com>
2022-01-10 17:06:03 +0700

    Finish project config

--- webpack.config.js
+++ webpack.config.js
@@ -80,12 +80,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        use: jsLoaders(),
       },
     ],
   },
